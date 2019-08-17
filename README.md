2418bac4514e7590e9e335bdfaede6e7c4f0116c
Pierrick Rouxel <pierrick.rouxel@me.com>
2019-08-17 15:12:43 +0200

    rTorrent daemon

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # iocage-plugin-rtorrent-flood
 Artifact file(s) for rTorrent Flood iocage plugin
 
+## Flood
+Default credentials :
+User : admin
+Password : admin
+
 ## Test
 ```sh
 iocage fetch --name rtorrent-flood vnet=on bpf=yes dhcp=on --branch master
--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# rTorrent startup script
+chmod 555 /usr/local/etc/rc.d/rtorrent
+sysrc -f /etc/rc.conf rtorrent_enable="YES"
+
 # Create flood folder
 mkdir -p /usr/local/etc/rtorrent/flood
 chmod 755 /usr/local/etc/rtorrent/flood
@@ -8,7 +12,7 @@ cd /usr/local/etc/rtorrent || exit 1
 # Download sources
 git clone https://github.com/Flood-UI/flood.git
 cd flood || exit 1
-mv /root/rtorrent/flood/config.js .
+mv -rf /root/rtorrent/flood/* .
 
 npm install
 npm run build
