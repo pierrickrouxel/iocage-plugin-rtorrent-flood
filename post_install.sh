bdeb6073a789422617094ceb8800cddf2cbce9de
fulder <misad90@gmail.com>
2021-03-13 16:50:40 +0100

    Remove PLUGIN_INFO

--- post_install.sh
+++ post_install.sh
@@ -3,6 +3,7 @@
 pw useradd rtorrent -g 0 -m
 pw useradd rtorrent_flood -g 0 -m
 
+mkdir -p /home/rtorrent_flood/flood
 chown rtorrent_flood /home/rtorrent_flood/flood
 
 npm install --global flood
@@ -15,14 +16,8 @@ sysrc -f /etc/rc.conf rtorrent_flood_enable="YES"
 service rtorrent start
 service rtorrent_flood start
 
-echo "Flood ui credentials" > /root/PLUGIN_INFO
-echo "User: admin" >> /root/PLUGIN_INFO
-echo "Password: admin" >> /root/PLUGIN_INFO
-
 # OpenVPN directory
 mkdir /usr/local/etc/openvpn
 
 # Define firewall rules
 sysrc -f /etc/rc.conf firewall_script="/etc/ipfw.rules"
-
-cat /root/PLUGIN_INFO
