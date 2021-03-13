6f926d070aceee27a2930c4f39d0f00d4800075e
fulder <misad90@gmail.com>
2021-03-13 16:37:52 +0100

    Add back user/password in PLUGIN_INFO

--- post_install.sh
+++ post_install.sh
@@ -16,7 +16,9 @@ sysrc -f /etc/rc.conf rtorrent_flood_enable="YES"
 service rtorrent start
 service rtorrent_flood start
 
-echo "Rtorrent socket path: /home/rtorrent/session/rtorrent.sock" > /root/PLUGIN_INFO
+echo "Flood ui credentials" > /root/PLUGIN_INFO
+echo "User: admin" >> /root/PLUGIN_INFO
+echo "Password: admin" >> /root/PLUGIN_INFO
 
 # OpenVPN directory
 mkdir /usr/local/etc/openvpn
